c0d33f220b79abc62b1246bf663acd76defd26ed
muyao <muyao1987@qq.com>
2020-10-08 19:31:29 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,34 @@
+# mars3d-vue-electron
+基于vueCli 4.x.x + 和 electron 6.x.x+ 的Mars3D平台CS桌面版程序模板开发模板
+ 
+
+##  项目安装（初次）
+```
+npm install / cnpm install 
+```
+
+##  CS桌面程序访问
+
+### 编译并热部署的Electron开发环境 (Compiles and hot-reloads for Electron development)
+```
+npm run serve / cnpm run serve
+```
+
+### 编译并压缩的Electron生产环境 (Compiles and minifies for Electron production)
+```
+npm run build / cnpm run build
+```
+
+
+
+##  BS浏览器访问
+
+### 编译并热部署的web浏览器开发环境 (Compiles and hot-reloads for development)
+```
+npm run serve-web / cnpm run serve-web
+```
+
+### 编译并压缩的web浏览器生产环境 (Compiles and minifies for production)
+```
+npm run build-web / cnpm run build-web
+```
---
+++ vue.config.js
@@ -0,0 +1,124 @@
+// vue.config.js 配置说明
+// 这里只列一部分，具体配置参考官方文档https://cli.vuejs.org/zh/config/#css-loaderoptions
+const webpack = require('webpack')
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const cesiumSource = 'node_modules/cesium/Build/Cesium'
+
+module.exports = {
+  publicPath: './',
+  assetsDir: './static',
+  productionSourceMap: false,
+  lintOnSave: false, // 是否开启eslint
+  devServer: {
+    open: true
+  },
+  configureWebpack: (config) => {
+    let plugins = []
+    if (process.env.NODE_ENV === 'production') {
+      plugins = [
+        new webpack.DefinePlugin({
+          CESIUM_BASE_URL: JSON.stringify('static')
+        }),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Workers'), to: 'static/Workers' }]),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Assets'), to: 'static/Assets' }]),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'ThirdParty'), to: 'static/ThirdParty' }]),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Widgets'), to: 'static/Widgets' }])
+      ]
+    } else {
+      plugins = [
+        new webpack.DefinePlugin({
+          CESIUM_BASE_URL: JSON.stringify('')
+        }),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Workers'), to: 'Workers' }]),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Assets'), to: 'Assets' }]),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'ThirdParty'), to: 'ThirdParty' }]),
+        new CopyWebpackPlugin([{ from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }])
+      ]
+    }
+    return {
+      module: {
+        unknownContextCritical: false,
+        rules: [
+          {
+            test: /\.js$/,
+            enforce: 'pre',
+            include: path.resolve(__dirname, 'node_modules/cesium/Source'),
+            sideEffects: false,
+            use: [
+              {
+                loader: 'strip-pragma-loader',
+                options: {
+                  pragmas: {
+                    debug: false
+                  }
+                }
+              }
+            ]
+          }
+        ]
+      },
+      optimization: {
+        usedExports: true,
+        splitChunks: {
+          maxInitialRequests: Infinity,
+          minSize: 0,
+          maxSize: 250000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10,
+              chunks: 'all',
+              name (module) {
+                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+                return `npm.${packageName.replace('@', '')}`
+              }
+            },
+            commons: {
+              name: 'Cesium',
+              test: /[\\/]node_modules[\\/]cesium/,
+              priority: 10,
+              chunks: 'all'
+            }
+          }
+        }
+      },
+      output: {
+        sourcePrefix: ' '
+      },
+      amd: {
+        toUrlUndefined: true
+      },
+      resolve: {
+        alias: {
+          vue$: 'vue/dist/vue.esm.js',
+          '@': path.resolve('src')
+        }
+      },
+      node: {
+        fs: 'empty',
+        Buffer: false,
+        http: 'empty',
+        https: 'empty',
+        zlib: 'empty'
+      },
+      plugins: plugins
+    }
+  },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: 'marsgis.cn', // 开发者标示
+        win: {
+          icon: 'mars3d.ico' // windows系统中icon需要256*256的ico格式图片，更换应用图标亦在此处
+        },
+        nsis: {
+          // nsis配置参数
+          oneClick: false, // 可单击打开
+          allowToChangeInstallationDirectory: true, // 允许用户选择安装位置
+          perMachine: true
+        }
+      }
+    }
+  }
+}
